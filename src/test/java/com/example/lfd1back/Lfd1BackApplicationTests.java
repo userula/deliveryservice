35b351aebeffc26a3916f1f4749276890703f76e
userula <ulan.buzhbanov620@gmail.com>
2021-04-02 15:18:21 +0600

    test1

---
+++ src/test/java/com/example/lfd1back/Lfd1BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lfd1back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lfd1BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
