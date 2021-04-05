72b3bbf3be396567bffc1a7ad8fda13773f529b7
userula <ulan.buzhbanov620@gmail.com>
2021-04-05 10:14:51 +0600

    test2

---
+++ src/main/java/com/example/lfd1back/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.example.lfd1back.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+}
